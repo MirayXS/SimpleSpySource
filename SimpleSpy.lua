8310eac70aeb0eba280474a7df97663ec9d62cfa
Laptop <superjamrs323@gmail.com>
2020-10-27 13:31:37 -0700

    added vector3 and cframe directly

--- SimpleSpy.lua
+++ SimpleSpy.lua
@@ -1153,6 +1153,10 @@ function u2s(u)
         return "nil --[[RBXScriptSignal]]"
     elseif typeof(u) == "PathWayoint" then
         return string.format("PathWaypoint.new(%s, %s)", v2s(u.Position), v2s(u.Action))
+    elseif typeof(u) == "Vector3" then
+        return string.format("Vector3.new(%s, %s, %s)", v2s(u.X), v2s(u.Y), v2s(u.Z))
+    elseif typeof(u) == "CFrame" then
+        return string.format("CFrame.new(%s, %s)", v2s(u.Position), v2s(u.LookVector))
     else
         return typeof(u) .. ".new(" .. tostring(u) .. ")"
     end
